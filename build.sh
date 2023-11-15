1136e2b3358fd5af16645375ad2515f041ab9806
endermn <viktorboyadjiev@gmail.com>
2023-11-15 12:16:05 +0200

    added src folder

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 set -e
 clear
-clang++ -std=c++2b -fsanitize=undefined -g *.cpp -o main -lSDL2 -lSDL2_image
+clang++ -std=c++2b -fsanitize=undefined -g ./src/*.cpp -o main -lSDL2 -lSDL2_image
 ./main
